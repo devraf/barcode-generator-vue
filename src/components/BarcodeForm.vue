1c406dd8d679e7ac64a7332be27b2dcd40f3a41b
devraf <rafael.perales@outlook.com>
2019-04-15 14:41:40 -0500

    initial commit

---
+++ src/components/BarcodeForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <form action="">
+    <label for="list">Add list</label>
+    <textarea name="list" id="list" cols="30" rows="10"></textarea>
+    <button>Create</button>
+    <button>123</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "BarcodeForm",
+  data() {
+    return {
+      test: 123
+    };
+  }
+};
+</script>
+
+<style></style>
---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <h1>hi</h1>
+    <BarcodeForm />
+  </main>
+</template>
+
+<script>
+import BarcodeForm from "@/components/BarcodeForm.vue";
+
+export default {
+  name: "Main",
+  components: {
+    BarcodeForm
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
